2e5546af2371d9e353d8863ffb04fd29673788e9
Leonardo Barcelos <leonardo.barcelos@syntesis.com.br>
2017-04-28 11:54:54 -0300

    Adicionado babel e scripts para rodar com forever

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+#!/usr/bin/env node
+'use strict';
+
+require('babel-register');
+
 var ping = require('ping');
 var fs = require('fs');
 
@@ -5,14 +10,13 @@ const hosts = ['facebook.com', 'google.com', 'yahoo.com'];
 const intervalSeconds = 60;
 
 setInterval(() => {
+    const date = new Date();
+    const time = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
     hosts.forEach((host) => {
         ping.sys.probe(host, (isAlive) => {
-            let date = new Date();
-            let msg = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()} >> ${host} ping failed!`
+            let msg = `${time} >> ${host} ping failed!`
             if (!isAlive) {
-                fs.appendFile('./fail.log', `${msg}\n`, () => {
-                    console.log(msg);
-                });
+                fs.appendFile('./fail.log', `${msg}\n`);
             }
         });
     });
